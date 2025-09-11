2d53d4916a2ccaf728c096001a895edc54468382
Luiz Felipe Frez <luiz.frez.santos@escola.pr.gov.br>
2025-09-11 14:53:52 +0000

    aula02

--- jogoAdivinha.c
+++ jogoAdivinha.c
@@ -1,5 +1,20 @@
 
 # include <stdio.h>
 int main(){
-    printf("Olá ");
+    
+
+    // Imprime o  cabeçalho do jogo.
+
+    printf ("___________________________________________\n");
+    printf ("/////////  Jogo de adivinhação   ////////// \n");
+    printf ("/////         Luiz Felipe             ///// \n");
+    printf ("/////////                        ////////// \n");
+    printf ("___________________________________________\n");
+
+
+    int numeroSecreto = 58;
+
+    printf("O número secreto é")
+
+
 }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
