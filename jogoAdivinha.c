6a9569f2f4ab74dddafddbab27f7d0961b74d543
Luiz Felipe Frez <luiz.frez.santos@escola.pr.gov.br>
2025-09-15 14:12:21 +0000

    aula do alura01

--- jogoAdivinha.c
+++ jogoAdivinha.c
@@ -11,10 +11,29 @@ int main(){
     printf ("/////////                        ////////// \n");
     printf ("___________________________________________\n");
 
+    int numerosecreto = 42;
+    
+    int chute;
 
-    int numeroSecreto = 58;
+    printf("Qual é o seu chute? ");
+    scanf("%d", &chute);
+    printf("Seu chute foi %d\n", chute);
 
-    printf("O número secreto é")
+    int acertou = (chute == numerosecreto);
 
+
+    if(acertou) {
+        printf("Parabéns! Você acertou!\n");
+        printf("Jogue de novo, você é um bom jogador!\n");
+    } 
+    else {
+
+        int maior = chute > numerosecreto; 
+        if(maior) {
+            printf("Seu chute foi maior que o número secreto\n");
+        } else {
+            printf("Seu chute foi menor que o número secreto\n");
+        }
+    }
 
 }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
