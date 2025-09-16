dafa5edb2a817f3b86d4d2130cc835df3bc3d135
Luiz Felipe Frez <luiz.frez.santos@escola.pr.gov.br>
2025-09-16 12:17:36 +0000

    terça aula 01

--- jogoAdivinha.c
+++ jogoAdivinha.c
@@ -12,28 +12,36 @@ int main(){
     printf ("___________________________________________\n");
 
     int numerosecreto = 42;
+
+    int numerodetentativas = 5
     
     int chute;
 
-    printf("Qual é o seu chute? ");
-    scanf("%d", &chute);
-    printf("Seu chute foi %d\n", chute);
+    for(int i = 1; i <= numerodetentativas; i++) {
 
-    int acertou = (chute == numerosecreto);
+        printf("Tentativa %d de %d\n", i, numerodetentativas);
+        printf("Qual é o seu chute? ");
 
+        scanf("%d", &chute);
+        printf("Seu chute foi %d\n", chute);
 
-    if(acertou) {
-        printf("Parabéns! Você acertou!\n");
-        printf("Jogue de novo, você é um bom jogador!\n");
-    } 
-    else {
+        int acertou = (chute == numerosecreto);
 
-        int maior = chute > numerosecreto; 
-        if(maior) {
-            printf("Seu chute foi maior que o número secreto\n");
-        } else {
-            printf("Seu chute foi menor que o número secreto\n");
+        if (acertou) {
+            printf("Parabéns! Você acertou!\n");
+            printf("Jogue de novo, você é um bom jogador!\n");
+
+            break;
+        }
+        else {
+            int maior = chute > numerosecreto;
+            if(maior) {
+                printf("Seu chute foi maior que o número secreto\n");
+            } else {
+                printf("Seu chute foi menor que o número secreto\n");
+
+            }
         }
     }
-
-}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+    printf("Fim de jogo! \n");
+}
